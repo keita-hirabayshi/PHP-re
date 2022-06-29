bfce6149816060a2fa824a9cc9a42d67ffebecc2
Keita Hirabayashi <178keita1034@gmail.com>
2022-06-30 07:36:07 +0900

    PHPに関する内容復習

--- restudy.php
+++ restudy.php
@@ -25,7 +25,7 @@ if(empty($t)){    echo'true';
 
 // 練習問題
 $products = [
-    'table' => [1000, 2], 
+    'table' => [1000, 2],
     'chair' => [500, 4],
     'bed' => [10000, 1],
     'light' => [5000, 1]
@@ -53,7 +53,7 @@ foreach ($cart as $name => $unit){
 
 /**
  * php専用のコメント枠(doc comment)
- * 
+ *
  * @author keita
  * @since 1.0.0
  */
@@ -70,7 +70,7 @@ $figure['key'] = $figure['key'] ?? 2;
  * constとdefineがある
  * 違いは、constの場合関数内で定義できない点。
  * if(!define($tex)){define($tax,0.1)}      定数を定義する時、重複しないようにするよく使うやり方
- * define(定数名,代入したい値) 
+ * define(定数名,代入したい値)
  */
 
  /**
@@ -105,55 +105,71 @@ $figure['key'] = $figure['key'] ?? 2;
 
 /**
  * データ型の宣言
- * 
+ *
  * function add (int $val): int{
  * return (int) ($val + 1);}
  * 引数だけでなく、関数の中身に対しても : int としてやることで処理内容にもデータ型を指定することができる。
  */
 
- 
+
 /**Webサーバー
  * apacheについて
  * webサーバーの一種で、情報をモジュール単位で追加・削除可能
  * 設定については、http.conf,.htaccessにてディレクティブごとに変更が可能
- * 
+ *
  * ディレクティブにはさまざまな種類がある。それらを記載可能な場所をコンテキストと呼ぶ
  * httpd.conf , virtualhost , .htaccess , Directory
- * 
+ *
  * alias → サーバー設定ファイルやヴァーチャルホスト     パスを置き換えショートカットできるようになる
  * ↓ 　適用範囲はディレクトリ以下の下位層に適用される
  * directory → ディレクトリ、サブディレクトリに対し、読み込みたいファイルや表示形式を指定できる
  * .htaccess → override all を有効にして使う。ディレクトリ、サブディレクトリに対し、.htaccesファイルを作成して適用する。(昨日はdirectoryと同様)
- * 
+ *
  * redirect(転送処理のこと)
  * redirect /apache/redirect /apache  指定のパスが該当すれば別のパスへ転送しり処理のこと(ブラウザのlocationでパスを伝える。)
  * 301と302リダイレクト　(301永続的で、一度リダイレクトするとブラウザ内に転送先をキャッシュとして保持する。ブラウザのキャッシュをクリアしない限り、サーバにて設定を変更しても反映されない。302一次的な引っ越しで有効)
  * 301 Redirect
  */
 
-/**
- * apache  ログの出力
- * Loglevel,ErrorLog,CustomLog,LogFormat
- * Loglevel → エラー出力の詳細度を指定　error > warn > debug
- * ErrorLog → エラーログの出力先パス
- * LogFormat → ログの出力内容について指定できる。
- * CustomLog → LogFormatで設定した内容を特定のログファイルにパスを用いて適用させる。
- * 
- * Rewriterule (ディレクトリ)
- * RewriteEngine を Onに設定してあげる必要がある。
- * redirectではブラウザとサーバー間でやりとりがあったが、こちらではapache内部で処理を行うのでパスの表記は変わらん
- * RewriteRule rewrite-test/index.html - [F]    → 対象のディレクトリに設定だけ追記することも可能。(Forbiddenでアクセスを禁止)
- * 
- * RewriteCond  RewriteRuleの検索範囲の詳細度を高めたもの(直下のRewriteRuleにのみ適用される。)
- * その１
- * RewriteCond %{HTTTP_HOST} localhost
- * RewriteRule rewrite-test/imgs/(\d{3}).jpg imgs/$1.png
- * その２
- * ReriteEngine On
- * RewriteBase apache/rewrite-test/
- * RewriteCond %{QUERY_STRING}} P=(.+)
- * RewriteRule rewrite-test/sub1 sub1/%1?
- * queryはURLの末尾に?から始めることで付与できる。
- * また、後方参照で読み込んだでのURLはそのままだと末尾にqueryが付き無限ループしてしまうので、文末に?を置き最終盤URLと認識させる
- * 
- */
+ /**
+  * apache  ログの出力
+  * Loglevel,ErrorLog,CustomLog,LogFormat
+  * Loglevel → エラー出力の詳細度を指定　error > warn > debug
+  * ErrorLog → エラーログの出力先パス
+  * LogFormat → ログの出力内容について指定できる。
+  * CustomLog → LogFormatで設定した内容を特定のログファイルにパスを用いて適用させる。
+  *
+  * Rewriterule (ディレクトリ)
+  * RewriteEngine を Onに設定してあげる必要がある。
+  * redirectではブラウザとサーバー間でやりとりがあったが、こちらではapache内部で処理を行うのでパスの表記は変わらん
+  * RewriteRule rewrite-test/index.html - [F]    → 対象のディレクトリに設定だけ追記することも可能。(Forbiddenでアクセスを禁止)
+  *
+  * RewriteCond  RewriteRuleの検索範囲の詳細度を高めたもの(直下のRewriteRuleにのみ適用される。)
+  * その１
+  * RewriteCond %{HTTTP_HOST} localhost
+  * RewriteRule rewrite-test/imgs/(\d{3}).jpg imgs/$1.png
+  * その２
+  * ReriteEngine On
+  * RewriteBase apache/rewrite-test/
+  * RewriteCond %{QUERY_STRING}} P=(.+)
+  * RewriteRule rewrite-test/sub1 sub1/%1?
+  * queryはURLの末尾に?から始めることで付与できる。
+  * また、後方参照で読み込んだでのURLはそのままだと末尾にqueryが付き無限ループしてしまうので、文末に?を置き最終盤URLと認識させる
+  *
+  * その３
+  * RewriteCond %{HTTP_ACCEPT} image/webp
+  * RewriteCond /Applications/MAMP/htdocs/fullstack-webdev/070_Apacheの基礎/rewrite-test/imgs/$1.webp -f
+  * RewriteCond /?imgs/(.*)\.(jpg?|png) imgs/$1.webp
+  */
+
+ /**
+  * データベース
+  * 表形式でのデータ管理が主流となっている。
+  * 個々のデータを識別するために、主キーPKの概念がある。これは、変化しない値が良いとされている。
+  * 複合キーで値を設定する場合には、外部キーFKとして別テーブルにて管理するのがよい(第三正規化まで)。(→ データを取得する場合には表を結合するjointを行う)
+  *
+  * テーブル設計
+  * 第一正規化   テーブル内の要素を一行ごとに設定し、主キーとなるものを選定する。
+  * 第二正規化   主キーに従属する属性、部分関数従属を別テーブルにて紐づける。外部キー設定。
+  * 第三正規化   主キー以外に従属する属性、推移的関数従属を別テーブルにて紐づける。基本ここまでで正規化は終了する。
+  */
