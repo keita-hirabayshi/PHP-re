16036632052915a82703ae8689ec2be30ec2b745
Keita Hirabayashi <178keita1034@gmail.com>
2022-08-21 21:15:02 +0900

    チャート図の導入

--- poll/index.php
+++ poll/index.php
@@ -15,3 +15,19 @@ require_once 'config.php';
     <p>サマーウォーズ</p>
 </body>
 </html>
+
+<!-- チャートの導入 -->
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <canvas id="chart" width="400" height="400" data-likes="10" data-dislikes="14"></canvas>
+  <script src="js/vendor/chart.js"></script>
+  <script src="js/pie-chart.js"></script>
+</body>
+</html>
